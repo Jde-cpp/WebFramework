582342288d5631379493fd5a14507b084cc19e5e
jde-cpp <johnduffyx@outlook.com>
2022-05-09 05:40:00 -0400

    add wrong dir message

--- jde-framework-proto.sh
+++ jde-framework-proto.sh
@@ -3,6 +3,10 @@ baseDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 jdeRoot=$baseDir/../..;
 source $jdeRoot/Framework/common.sh;
 pushd `pwd` > /dev/null;
+if [ ! -d node_modules ]; then
+	echo run from workspace dir.
+	exit 1;
+fi;
 cd node_modules;
 moveToDir jde-cpp;
 error()
@@ -11,9 +15,9 @@ error()
     exit $errorCode;
 }
 trap error ERR;
-mklink FromClient.proto $jdeRoot/Public/jde/log/types/proto; 
+mklink FromClient.proto $jdeRoot/Public/jde/log/types/proto;
 mklink FromServer.proto $jdeRoot/Public/jde/log/types/proto;
-npx pbjs -r app_from_client -t static-module -w es6 -o FromClient.js FromClient.proto; 
+npx pbjs -r app_from_client -t static-module -w es6 -o FromClient.js FromClient.proto;
 npx pbts -o FromClient.d.ts FromClient.js;
 npx pbjs -r app_from_server -t static-module -w es6 -o FromServer.js FromServer.proto;
 npx pbts -o FromServer.d.ts FromServer.js;
